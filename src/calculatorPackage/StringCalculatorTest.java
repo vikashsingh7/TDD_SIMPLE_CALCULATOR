277e044798e5fe12aa7a5b41d57c099bd9cb8b20
Vikash <vikashsngh580@gmail.com>
2020-10-16 01:01:23 +0530

    Fourth Commit
    Added test to check unknown numbers of inputs in the String,

--- src/calculatorPackage/StringCalculatorTest.java
+++ src/calculatorPackage/StringCalculatorTest.java
@@ -22,4 +22,14 @@ public class StringCalculatorTest {
         assertEquals(50, StringCalculator.add("10,40"));
         assertEquals(100, StringCalculator.add("1,99"));
     }
+
+    @Test
+    public void shouldHandleUnknownAmountOfNumbers(){
+        assertEquals(999999999, StringCalculator.add("999999999"));
+        assertEquals(999999999, StringCalculator.add("931531511,68468488"));
+        assertEquals(444, StringCalculator.add("93,68,99,151,33"));
+        assertEquals(7100, StringCalculator.add("9,37,6,87,6873,6,8,14,60"));
+        assertEquals(98683, StringCalculator.add("9,37,6,87,6873,6,8,14,60,45,4,23,2345,34,3434,85667,6,22,3"));
+    }
+
 }
