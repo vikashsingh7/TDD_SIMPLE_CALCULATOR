5f50aaa7352ab51c5c0e480f9463a4e9e356a644
Vikash <vikashsngh580@gmail.com>
2020-10-16 00:28:39 +0530

    Third Commit
    Added the check for Delimiter.
    Checking only "," for now.

--- src/calculatorPackage/StringCalculator.java
+++ src/calculatorPackage/StringCalculator.java
@@ -8,15 +8,27 @@ public class StringCalculator {
         if(numbers.isEmpty()){
             return 0;
         }
+        else if(numbers.contains(",")){
+            String[] numberArray = splitNumbers(numbers,",");
+            int result=0;
+            for (String item: numberArray) {
+                result += Integer.parseInt(item);
+            }
+            return result;
+        }
         else{
             return fetchNumbersFromString(numbers);
         }
     }
 
-    public static int fetchNumbersFromString(String text){
+    private static int fetchNumbersFromString(String text){
         if(text.contains(" ")){
             text = text.replaceAll(" ","");
         }
         return Integer.parseInt(text);
+    }
+
+    private static String[] splitNumbers(String text, String divider){
+        return text.split(divider);
     }
 }
--- src/calculatorPackage/StringCalculatorTest.java
+++ src/calculatorPackage/StringCalculatorTest.java
@@ -16,4 +16,10 @@ public class StringCalculatorTest {
         assertEquals(789, StringCalculator.add("7 8 9"));
         assertEquals(789, StringCalculator.add("789"));
     }
+
+    @Test
+    public void shouldAddNumbersIfDelimiterPresent(){
+        assertEquals(50, StringCalculator.add("10,40"));
+        assertEquals(100, StringCalculator.add("1,99"));
+    }
 }
