ed3186f66b49aac57b33e7d5af782769e3d6e68a
Vikash <vikashsngh580@gmail.com>
2020-10-16 12:12:58 +0530

    Code_Optimisation_v1
    Changed the behaviour of add function to make it more readable and faster.

--- src/calculatorPackage/StringCalculator.java
+++ src/calculatorPackage/StringCalculator.java
@@ -1,6 +1,7 @@
 package calculatorPackage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StringCalculator {
 
@@ -11,46 +12,40 @@ public class StringCalculator {
         if(numbers.isEmpty()){
             return 0;
         }
-        if(numbers.contains("\n")){
-            numbers = replaceSymbolsFromString(numbers,"\n",delimiter);
-        }
-        if(numbers.contains(" ")){
-            numbers = replaceSymbolsFromString(numbers," ","");
-        }
-        if(numbers.startsWith("//")){
-            delimiter = Character.toString(numbers.charAt(2));
-            numbers = numbers.substring(4);
-        }
-        if(!numbers.contains(delimiter)){
-            try{
-                return Integer.parseInt(numbers);
+        else{
+            numbers = numbers.replaceAll(" ","");   //Replacing Blank Spaces
+            if(numbers.startsWith("//")){
+                delimiter = Character.toString(numbers.charAt(2));
+                numbers = numbers.substring(4);
             }
-            catch(Exception e){
-                System.out.println("Some Other Symbol Present");
-            }
+            numberArray = splitNumbers(numbers,delimiter);
+            return add(numberArray);
         }
-        numberArray = splitNumbers(numbers,delimiter);
-        return add(numberArray);
     }
 
-    private static String replaceSymbolsFromString(String text, String fromSymbol, String toSymbol){
-        if(text.contains(fromSymbol)){
-            text = text.replaceAll(fromSymbol, toSymbol);
-        }
-        return text;
-    }
+//    private static String replaceSymbolsFromString(String text, String fromSymbol, String toSymbol){
+//        if(text.contains(fromSymbol)){
+//            text = text.replaceAll(fromSymbol, toSymbol);
+//        }
+//        return text;
+//    }
 
     private static int add(String textArray[]){
         int result=0;
         for (String item: textArray) {
             if(!item.isEmpty()) {
-                result += Integer.parseInt(item);
+                try {
+                    result += Integer.parseInt(item);
+                }
+                catch (Exception e){
+                    System.out.println("Illegal Symbol Present!");
+                }
             }
         }
         return result;
     }
 
     private static String[] splitNumbers(String text, String divider){
-        return text.split(divider);
+        return text.split("[\n" + divider + "]");
     }
 }
